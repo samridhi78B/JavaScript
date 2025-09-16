0e8003d170585c26e0b0975f1a0e66450b7a4709
Samridhi Gumber <samridhigumber05@gmail.com>
2025-09-16 18:05:05 +0530

    Create script.js

---
+++ temperature conversion/script.js
@@ -0,0 +1,20 @@
+const text=document.getElementById('text');
+const toFahrenhiet=document.getElementById('toFahrenhiet');
+const toCelsius=document.getElementById('toCelsius');
+const result=document.getElementById('result');
+let temp;
+function convert(){
+    if(toFahrenhiet.checked){
+        temp=Number(text.value);
+        temp = temp*9/5+32;
+        result.textContent = temp.toFixed(1) + "F";
+    }
+    else  if(toCelsius.checked){
+        temp=Number(text.value);
+        temp = (temp-32) * (5/9);
+        result.textContent = temp.toFixed(1) + "C";
+    }
+    else{
+        result.textContent ="Select a unit";
+    }
+}
