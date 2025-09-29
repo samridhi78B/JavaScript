58e2f9584761607f1f1a4740b6aab519ce1a5ba8
Samridhi Gumber <samridhigumber05@gmail.com>
2025-09-29 17:09:25 +0530

    Create map,filter,reduce function.js

---
+++ map,filter,reduce function.js
@@ -0,0 +1,24 @@
+// .map() = accepts a callback and applies that function to each element of an array and return a new array
+// const number=[1,2,3,4,5];
+// const sq = number.map(square);
+// console.log(sq); // [1, 4, 9, 16, 25]
+// function square(element){
+//   return Math.pow(element,2);
+// }
+
+
+// .filter() = create new array by filtering out elements
+// const number=[1,2,3,4,5];
+// const nums = number.filter(odd);
+// console.log(nums);  //[1, 3, 5]
+// function odd(element){
+//   return element % 2 !=0;
+// }
+
+// .reduce() = reduce elements of array to a single value
+// const prices = [5,10,20,100,45,30];
+// const p = prices.reduce(sum);
+// console.log(p);
+// function sum(previous,element){
+//   return previous + element;
+// }
