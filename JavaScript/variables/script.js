e09555529610657ebf2696e68afb60f8f8c519fa
Samridhi Gumber <samridhigumber05@gmail.com>
2026-08-14 12:43:32 +0530

    Add files via upload

---
+++ JavaScript/variables/script.js
@@ -0,0 +1,23 @@
+
+const name="Avantika";
+let age=25;
+const college="DU";
+const course="BA";
+console.log(name); //Avantika
+console.log(age); //25
+console.log(college); //DU
+console.log(course); //BA
+
+
+let marks=70;
+marks=95;
+console.log(marks); //95
+
+const birthYear=2006;
+const calc_age=new Date().getFullYear()-birthYear;
+console.log(calc_age); //20
+
+const price=500;
+const quantity=3;
+const totalPrice = price*quantity;
+console.log(totalPrice); //1500
